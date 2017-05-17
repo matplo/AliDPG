fc476ec7d4c233b489ef03d77e3f35596f9f299d
matplo <ploskon@gmail.com>
2017-05-16 17:36:44 -0700

    run test ok

--- MC_muon_calo/run_tests.sh
+++ MC_muon_calo/run_tests.sh
@@ -7,9 +7,9 @@
 
 export ALIDPG_ROOT=/global/homes/p/ploskon/software/alice/AliDPG
 
-# this is running tests
-make RUN=267020
+# this is running tests - including making the snapshot in case not already present
+# make RUN=267020
 
 # this is actual simulation
-./dpgsim.sh --mode full --run 267020 --generator Custom --energy 8000 --pthardbin 1 --nevents 5 --ocdb snapshot --detector Custom --simulation Custom --reconstruction Custom --system p-Pb
-
+./dpgsim.sh --mode full --run 267020 --generator Custom --energy 8000 --pthardbin 1 --nevents 5 --ocdb snapshot --simulation Custom --reconstruction Custom --system p-Pb --trigger p-p
+# --detector Custom
